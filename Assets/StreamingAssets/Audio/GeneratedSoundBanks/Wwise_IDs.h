2af5d61e422f2701aaf8a28a8e17b5b68c12f581
ThunderJams <57014565+ThunderJams@users.noreply.github.com>
2023-01-05 12:30:28 +0000

    Textures

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,6 +13,8 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID CARPETFOOTSTEPSF = 4077091701U;
+        static const AkUniqueID CARPETSTEPS = 3720131405U;
         static const AkUniqueID FOOTSTEPS = 2385628198U;
         static const AkUniqueID WOODSTEPS = 2593838623U;
     } // namespace EVENTS
